0bbd89f6ee5fd0ef079a0a6a70fb3695054c14e3
Atolagbe David Tolulope <atolagbedt@gmail.com>
2024-05-13 23:01:36 +0100

    data fetching with useQuery

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useQuery } from '@tanstack/react-query';
 function App() {
+  const { data, isLoading, error } = useQuery({
+    queryKey: ['todos'],
+    queryFn: () =>
+      fetch('https://jsonplaceholder.typicode.com/todos').then((res) =>
+        res.json()
+      ),
+  });
+
+  if (isLoading) return <div>Loading</div>;
+  if (error) return <div>An error occured</div>;
   return (
-    <div className="App">
-      
+    <div>
+      {data.map((todo, idx) => (
+        <div key={idx}>
+          <h4>{todo.id}</h4>
+          <h1>{todo.title}</h1>
+        </div>
+      ))}
     </div>
   );
 }
