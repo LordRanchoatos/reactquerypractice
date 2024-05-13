771cf9539481eb0d1ff069b4816f5a876a6c9223
Atolagbe David Tolulope <atolagbedt@gmail.com>
2024-05-13 23:33:59 +0100

    useMutation

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery } from '@tanstack/react-query';
 function App() {
   const { data, isLoading, error } = useQuery({
     queryKey: ['todos'],
@@ -8,10 +8,32 @@ function App() {
       ),
   });
 
+  const { mutate, isPending, isError, isSuccess } = useMutation({
+    mutationFn: (newPost) =>
+      fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        body: JSON.stringify(newPost),
+        headers: { 'Content-type': 'application/json; charset=UTF-8' },
+      }).then((res) => res.json()),
+  });
+
   if (isLoading) return <div>Loading</div>;
-  if (error) return <div>An error occured</div>;
+  if (error || isError) return <div>An error occured</div>;
   return (
     <div>
+      {isPending && <p>yor post is updating!!</p>}
+      <button
+        onClick={() =>
+          mutate({
+            userId: 1000,
+            id: 1989,
+            title: 'new post leggo go!',
+            body: 'this is a new post, let, trying it out!',
+          })
+        }
+      >
+        New post
+      </button>
       {data.map((todo, idx) => (
         <div key={idx}>
           <h4>{todo.id}</h4>
